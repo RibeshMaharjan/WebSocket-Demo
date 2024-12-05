eefcd633a567de098068c78e406b526c7593e712
AsuraMzn <ribubaucha@gmail.com>
2024-12-05 16:38:25 +0545

    manually handle upgrade
    Manually handle the upgrade from http connection to websocket connection\n*Created websocket server that can handle multple user

--- server/server.js
+++ server/server.js
@@ -1,42 +1,61 @@
 // http server
-const { log } = require('console');
-const http = require('http'); 
+const http = require('http');
 
 // WebSocket Server
-const WebSocketServer = require('websocket').server; 
-let connection = null;
-
+const WebSocket = require('ws');
+const WebSocketServer = WebSocket.WebSocketServer; 
 
 const httpServer = http.createServer((request, response) => {
   console.log("We have received a request");
 });
-
-const webSocket = new WebSocketServer({
-  "httpServer": httpServer
-});
-
-webSocket.on('request', request => {
-  
-  connection = request.accept(null, request.origin);
-  
-  console.log('Client connected!');
-  
-  connection.on('open', () => console.log('Opened!!'));
-
-  connection.on('close', () => {
-    console.log('Client disconnected!!')
-  });
-
-  connection.on('message', (message) => {
-    console.log(`Received message: ${message.utf8Data}`);
-    connection.send('Hey Client');
-  });
-}) 
-
 
 httpServer.listen(8080, () => {
   console.log("Server is listening to port 8080");
   
 })
 
+function onSocketPreError(e) {
+  console.log(e);
+}
 
+function onSocketPostError(e) {
+  console.log(e); 
+}
+
+
+const webSocketServer = new WebSocketServer({ noServer: true });
+
+httpServer.on('upgrade', (req, socket, head) => {
+  socket.on('error', onSocketPreError);
+
+  // perform auth if need
+
+  webSocketServer.handleUpgrade(req, socket, head, ws => {
+    socket.removeListener('error', onSocketPreError);
+    webSocketServer.emit('connection', ws, req);
+  });
+
+  
+});
+
+
+webSocketServer.on('connection', (ws, req) => {
+  ws.on('error', onSocketPostError);
+  console.log('Client connected');
+  
+  // receive client message
+  ws.on('message', (message, isBinary) => {
+    // pass msg to all the users in server
+    webSocketServer.clients.forEach(client => {
+      if(client.readyState === WebSocket.OPEN) {
+        client.send(message, { binary: isBinary });
+      }
+    })
+    
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  })
+
+})
